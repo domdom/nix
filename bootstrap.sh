349a53706c6dc7185786b292782f390d912b75ce
domdom <domdom@users.noreply.github.com>
2019-06-13 19:08:55 +1000

    Cleanup bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,23 @@
 
 # install nix
 # this depends on os, if we are on nix-os, this is not required
-[ ! -d /nix ] && curl https://nixos.org/nix/install | sh
-
-source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+[ ! -d /nix ] && sh <(curl https://nixos.org/nix/install) --daemon
 
 mkdir -p "$HOME/.config/nixpkgs"
 
-host=$(nix-instantiate --eval host.nix | xargs echo)
+prefix=${1}
 
-ln -srf machines/$host-config.nix "$HOME/.config/nixpkgs/config.nix"
-ln -srf machines/$host-home.nix "$HOME/.config/nixpkgs/home.nix"
-#nix-shell -v -p home-manager --run "home-manager switch"
+config="machines/$prefix-config.nix"
+if [ -e "$config" ]; then
+    ln -sf "$(realpath "$config")" "$HOME/.config/nixpkgs/config.nix"
+fi
+
+home="machines/$prefix-home.nix"
+if [ -e "$home" ]; then
+    ln -sf "$(realpath "$home")" "$HOME/.config/nixpkgs/home.nix"
+else
+    echo "'$home' does not exist!"
+    exit 1
+fi
+
+nix-shell -v -p home-manager --run "home-manager switch"
