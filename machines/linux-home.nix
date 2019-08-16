7e3c84ee97e0569c9ae844a4347a782bfdfdc350
domdom <domdom@users.noreply.github.com>
2019-08-16 13:48:29 +1000

    Fix java applications in BSPWM

--- machines/linux-home.nix
+++ machines/linux-home.nix
@@ -20,5 +20,8 @@
 
   home.sessionVariables = {
     LOCALE_ARCHIVE_2_27 = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+    # Trick to get java applications to render properly in BSPWM
+    # https://wiki.archlinux.org/index.php/Java#Non-reparenting_window_managers_/_Grey_window_/_Programs_not_drawing_properly
+    _JAVA_AWT_WM_NONREPARENTING = 1;
   };
 }
